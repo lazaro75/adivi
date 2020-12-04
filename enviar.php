d017f5afa2c58427fc3a165e66b5e3f6d59e866c
lazaro <lazaro.doc@hotmail.com.ar>
2020-12-04 14:41:48 -0200

    adiv

---
+++ enviar.php
@@ -0,0 +1,8 @@
+<?php
+$nombre = $_POST{"nombre y apellido"};
+echo $nombre. "ha dicho:<br/>". $mensaje);
+?>
+
+
+
+
---
+++ intro.js
@@ -0,0 +1,24 @@
+
+function guardar() {
+   event.preventDefault();
+    var nombre = document.getElementById("txtMateria").Value;
+
+    if(nombre == "");{
+        alert("no puedeokar vacio");
+        return;  
+
+    }
+
+    }
+    function guardar1() {
+        event.preventDefault();
+         var nombre = document.getElementById("txtMateria").Value;
+     
+         if(nombre == "");{
+             alert("no puedeokar vacio");
+             return;  
+     
+         }
+     
+         }
+    
